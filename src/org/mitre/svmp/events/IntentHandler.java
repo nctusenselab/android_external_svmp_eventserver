a675a929eb7a137dede9245e1518716be813f971
FatMinMin <chiehmin18@gmail.com>
2016-07-05 16:13:17 +0800

    adding support for ACTION_SEND intents

--- src/org/mitre/svmp/events/IntentHandler.java
+++ src/org/mitre/svmp/events/IntentHandler.java
@@ -86,29 +86,36 @@ public class IntentHandler extends BaseHandler {
 			Log.d(TAG, "Forwarding intent. Timestamp: " + timeStamp + " " + System.currentTimeMillis());
 
             SVMPProtocol.Intent intentRequest = request.getIntent();
+			Intent intent = new Intent();
             if(intentRequest.getAction().equals(SVMPProtocol.IntentAction.ACTION_VIEW)) {
-				Intent intent = new Intent(Intent.ACTION_VIEW);
-				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				if( intentRequest.hasFile() ) {
-					SVMPProtocol.File f = intentRequest.getFile();
-					Log.e(TAG, "Receiving a file with filename: " + f.getFilename());
-					saveToFile(f);
-					File savedFile = new File("/sdcard/" + f.getFilename());
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse(intentRequest.getData()));
+            } else if(intentRequest.getAction().equals(SVMPProtocol.IntentAction.ACTION_SEND)) {
+				intent.setAction(Intent.ACTION_SEND);
+			}
+			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			if( intentRequest.hasFile() ) {
+				SVMPProtocol.File f = intentRequest.getFile();
+				Log.e(TAG, "Receiving a file with filename: " + f.getFilename());
+				saveToFile(f);
+				File savedFile = new File("/sdcard/" + f.getFilename());
 
-					int idx = savedFile.getName().lastIndexOf('.');
-					String type = "*/*";
-					if (idx > 0) {
-					    String extension = savedFile.getName().substring(idx+1);
-						type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
-					}
-					Log.e(TAG, "MIME type is " + type);
-
-					intent.setDataAndType(Uri.fromFile(savedFile), type);
-				} else {
-					intent.setData(Uri.parse(intentRequest.getData()));
+				int idx = savedFile.getName().lastIndexOf('.');
+				String type = "*/*";
+				if (idx > 0) {
+					String extension = savedFile.getName().substring(idx+1);
+					type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
 				}
-                baseServer.getContext().startActivity(intent);
-            }
+				Log.e(TAG, "MIME type is " + type);
+
+				if(intentRequest.getAction().equals(SVMPProtocol.IntentAction.ACTION_VIEW)) {
+					intent.setDataAndType(Uri.fromFile(savedFile), type);
+	            } else if(intentRequest.getAction().equals(SVMPProtocol.IntentAction.ACTION_SEND)) {
+					intent.setType(type);
+					intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(savedFile));
+				}
+			}
+			baseServer.getContext().startActivity(intent);
         }
     }
 
